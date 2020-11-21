f2a9ab1bc113895037f854d5334d88d732185b08
John <5445550+johnfg2610@users.noreply.github.com>
2020-11-21 20:35:45 +0000

    Added pretty logger and prod logger

--- zerologx/zerologx.go
+++ zerologx/zerologx.go
@@ -1,1 +1,20 @@
 package zerologx
+
+import (
+	"os"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func DefaultLoggerContext() zerolog.Context {
+	return zerolog.New(os.Stderr).With().Timestamp()
+}
+
+func PrettyLogger() {
+	log.Logger = DefaultLoggerContext().Logger().Output(zerolog.ConsoleWriter{Out: os.Stderr})
+}
+
+func ProductionLogger() {
+	log.Logger = DefaultLoggerContext().Logger()
+}
